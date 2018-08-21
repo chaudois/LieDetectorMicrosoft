ec05cdabac1166317188ec7e3fa95816c759c037
Chaudois <damien.chaudois@gmail.com>
2018-08-21 15:36:30 +0200

    Ajout du Projet console ( avec Unity) pour vérifier la modularité de la BLL

---
+++ ConsoleUI/UnityConfig.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Unity;
+using BLL;
+using BLL.Interfaces;
+using Unity.Resolution;
+
+namespace ConsoleUI
+{
+    class UnityConfig
+    {
+        public static IUnityContainer Setup()
+        {
+
+            var result = new UnityContainer();
+            result.RegisterType<IFaceRecognizer, FaceRecognizer>();
+            result.RegisterType<IVideoSplitter, VideoSplitter>();
+            result.RegisterType<IVideoProvider, VideoProviderConsole>();
+            return result;
+
+        }
+    }
+}
